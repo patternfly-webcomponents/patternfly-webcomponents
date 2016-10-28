ebd835298259c1a2488a2861e64f6bc7c4192d61
David Taylor <dtaylor113@gmail.com>
2016-10-28 12:56:17 -0400

    Utiliz. Bar Chart: Inline layout & Threshold eventing

--- src/pf-utilization-bar-chart.js
+++ src/pf-utilization-bar-chart.js
@@ -1,31 +1,48 @@
 (function(){
   var doc = (document._currentScript || document.currentScript).ownerDocument;
   var utilBarChartTemplate = doc.querySelector('.utilization-bar-chart-template');
+  var lastThresholdClass;
+  var layout;
   var forEach = Array.prototype.forEach;
 
   //Pf-Utilization Bar Chart Custom Element
   class PfUtilizationBarChart extends HTMLElement {
     attachedCallback() {
+      layout = this.getAttribute('layout');
+      if(layout && layout === 'inline') {
+        utilBarChartTemplate = doc.querySelector('.utilization-bar-chart-inline-template');
+      }
       this.appendChild(document.importNode(utilBarChartTemplate.content, true));
-      var usedBar = this.querySelector('.progress-bar-used');
-      var remainingBar = this.querySelector('.progress-bar-remaining');
+      this.updateChart();
+    }
 
+    attributeChangedCallback(attributeName, oldValue, newValue) {
+      this.updateChart();
+    }
+
+    updateChart() {
       var chartTitle = this.getAttribute('chart-title');
       if(chartTitle) {
         this.querySelector('.progress-description').innerText = chartTitle;
       }
 
+      var usedBar = this.querySelector('.progress-bar-used');
+      var remainingBar = this.querySelector('.progress-bar-remaining');
+
       var usedValue = this.getAttribute('used');
       var totalValue = this.getAttribute('total');
       var units = this.getAttribute('units') !== null ? this.getAttribute('units') : "";
-      usedBar.querySelector('#labelTopRight').innerText = usedValue + " of " + totalValue + " " + units;
+
+      if(layout && layout === 'inline') {
+        usedBar.querySelector('.utiliz-bar-strong-label').innerText = usedValue + " " + units;
+      } else {
+        usedBar.querySelector('.utiliz-bar-strong-label').innerText = usedValue + " of " + totalValue + " " + units;
+      }
 
       var percentageUsed = Math.round(100 * (usedValue / totalValue));
 
       usedBar.setAttribute("style", "width: " + percentageUsed + "%;");
-      //usedBar.querySelector('.tooltiptext').innerText = percentageUsed + "% Used";
       remainingBar.setAttribute("style", "width: " + (100 - percentageUsed) + "%;");
-      //remainingBar.querySelector('.tooltiptext').innerText = (100 - percentageUsed) + "% Available";
 
       var errorThreshold = this.getAttribute('threshold-error');
       var warnThreshold = this.getAttribute('threshold-warning');
@@ -35,7 +52,7 @@
       }
     }
 
-    setUsedBarThresholdColor(usedBar, percentageUsed, errorThreshold, warnThreshold){
+    setUsedBarThresholdColor(usedBar, percentageUsed, errorThreshold, warnThreshold) {
       var thresholdClass;
 
       if (errorThreshold || warnThreshold) {
@@ -47,8 +64,13 @@
           thresholdClass = "progress-bar-success";
         }
 
-        if(thresholdClass){
-          usedBar.setAttribute("class", usedBar.getAttribute("class") + " " + thresholdClass);
+        if(thresholdClass != lastThresholdClass){
+          var event = new CustomEvent('thresholdSet', {'threshold':thresholdClass});
+          event.initCustomEvent('thresholdSet', true, true, {'threshold':thresholdClass});
+          usedBar.classList.remove(lastThresholdClass);
+          usedBar.classList.add(thresholdClass);
+          lastThresholdClass = thresholdClass;
+          usedBar.dispatchEvent(event);
         }
       }
     }

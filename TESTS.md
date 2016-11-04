303f48e283f543a271acb4ce9d1b1c2de09d4202
Patrick Riley <priley86@gmail.com>
2016-11-04 08:55:53 -0400

    PTNFLY 1868 Setup a perf test harness

---
+++ TESTS.md
@@ -0,0 +1,12 @@
+# Testing Patternfly-Webcomponents
+
+### Unit Tests
+
+### Performance Tests
+This project makes use of [Sitespeed.io](https://www.sitespeed.io/) for web performance tests. 
+
+Start a performance run with:
+```
+$ gulp perf
+```
+Once finished, review results in the `perf` directory.
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp'),
   browserSync = require('browser-sync').create(),
+  browserSyncPort = 3000,
+  baseUrl = 'http://localhost:' + browserSyncPort,
   eslint = require('gulp-eslint'),
   ignore = require('gulp-ignore'),
   karma = require('karma').Server,
   path = require('path'),
   sass = require('gulp-sass'),
   $ = require('gulp-load-plugins')(),
-  webpack = require('webpack-stream');
+  webpack = require('webpack-stream'),
+  runSequence = require('run-sequence'),
+  sitespeedio = require('gulp-sitespeedio');
 
 gulp.task('font', function(){
   return gulp.src([
@@ -54,6 +58,33 @@ gulp.task('test-debug', function (done) {
   }, done).start();
 });
 
+gulp.task('sitespeedio', ['serve'], function(done){
+  var run = sitespeedio({
+    urls: [
+      baseUrl + '/app/app.html?file=pf-alert',
+      baseUrl + '/app/app.html?file=pf-tabs',
+      baseUrl + '/app/app.html?file=pf-utilization-bar-chart'
+    ],
+    browser: 'chrome',
+    resultBaseDir: './perf/',
+    connection: 'cable',
+    html: true,
+    budget: {
+      page: {
+        pageWeight:1000000,
+        imageWeight: 300000,
+        jsWeight: 500000,
+        cssWeight: 500000
+      }
+    }
+  });
+  run(done);
+});
+
+gulp.task('perf', function(done){
+  runSequence('build', 'sitespeedio', done)
+});
+
 gulp.task('webpack', ['js'], function() {
   return gulp.src(['dist/es2015/*/*.js'])
     .pipe(webpack({
@@ -75,7 +106,8 @@ gulp.task('serve', function() {
   browserSync.init({
     server: {
       baseDir: './'
-    }
+    },
+    port: browserSyncPort
   });
 
   gulp.watch('index.html', ['build']);

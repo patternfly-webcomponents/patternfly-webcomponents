27300527f1a40d2e11d068509b5795c29faedeb9
Dan <dlabrecq@redhat.com>
2016-11-18 22:26:03 -0500

    Removing dist directory for clarity during PR reviews.

--- dist/es2015/pf-tabs/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-/** PF Tabs Component **/
-require('pf-tabs.component');
--- dist/es2015/pf-utils/pf-utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var PfUtils = {
-  // ToDo: Add utils here
-};
-exports.default = PfUtils;

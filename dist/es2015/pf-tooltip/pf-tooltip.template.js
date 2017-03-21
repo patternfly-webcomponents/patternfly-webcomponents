ef4adf3cf119ca378440793c17ec18068cd0c01a
patternfly-build <patternfly-build@redhat.com>
2017-03-21 19:10:34 +0000

    Added files generated by Travis build

---
+++ dist/es2015/pf-tooltip/pf-tooltip.template.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var PfTooltipTemplate = "\n<div role=\"tooltip\" class=\"tooltip\">\n    <div class=\"tooltip-arrow\"></div>\n    <div class=\"tooltip-inner\"></div>\n</div>\n";
+
+exports.default = PfTooltipTemplate;

32ab9fb9657c1cd0c4b10c64990b512e04e82c33
sahil143 <sahilbudhwar143@gmail.com>
2017-03-22 21:04:45 +0530

    Update test 3

--- src/pf-dropdown/pf-dropdown.spec.js
+++ src/pf-dropdown/pf-dropdown.spec.js
@@ -1,17 +1,25 @@
 describe("Patternfly Dropdown Component Test", function () {
   var customElement;
 
-  beforeEach(function () {
+  beforeEach(function (done) {
     customElement = document.createElement('pf-dropdown');
     customElement.innerHTML = '<button class="btn btn-primary dropdown-toggle" type="button" id="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropdown<span class="caret"></span></button><ul class="dropdown-menu"><li class="dropdown-header">Header</li><li><a href="#">Item 1</a></li><li><a href="#">Item 2</a></li><li class="disabled"><a href="#">Item 3</a></li><li><a href="#">Item 4</a></li><li class="divider"></li><li><a href="#">Item 5</a></li></ul>';
     document.body.appendChild(customElement);
+    if (customElement.initialized) {
+      done();
+    } else {
+      //wait for initialization
+      customElement.addEventListener('initialized', function (event) {
+        done();
+      });
+    }
   });
 
   afterEach(function () {
     document.body.removeChild(customElement);
   });
 
-  it("should open if closed or close if open the dropdown on toggle()", function () {
+  it("should open if closed or close if open the dropdown on toggle()", function (done) {
     var button = customElement.querySelector('#button');
     //open dropdown
     customElement.toggle();
@@ -25,6 +33,7 @@ describe("Patternfly Dropdown Component Test", function () {
     customElement.addEventListener('hidden.bs.dropdown', function () {
       expect(button.parentNode.classList.contains('open')).toBe(false);
     }, false);
+    done();
   });
 
   it("disabled button should not open dropdown", function () {
@@ -45,11 +54,7 @@ describe("Patternfly Dropdown Component Test", function () {
     customElement.addEventListener('itemClicked', function () {
       result = true;
     });
-    item.dispatchEvent(new MouseEvent('click',{
-      bubbles:true,
-      cancelable:true,
-      view: window
-    }));
+    item.click();
     expect(result).toBe(true);
   });
 

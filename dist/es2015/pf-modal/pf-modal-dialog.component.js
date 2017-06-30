f3ae9873c1fbe2bd9ffef1f27f25023f5cb7fd51
patternfly-build <patternfly-build@redhat.com>
2017-06-30 15:02:27 +0000

    Added files generated by Travis build

---
+++ dist/es2015/pf-modal/pf-modal-dialog.component.js
@@ -0,0 +1,56 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.PfModalDialog = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _pfUtils = require('pf-utils.js');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/*
+ * <b>&lt;pf-modal-dialog&gt;</b> element for Patternfly Web Components
+ *
+ * @example {@lang xml}
+ * <pf-modal targetSelector="#btn-toggle-modal" backdrop keyboard>
+ *  <pf-modal-dialog>
+ *   <pf-modal-content> practical content of pf-modal </pf-modal-content>
+ *  </pf-modal-dialog>
+ * </pf-modal>
+ */
+
+var PfModalDialog = exports.PfModalDialog = function (_HTMLElement) {
+  _inherits(PfModalDialog, _HTMLElement);
+
+  /*
+   * An instance of the element is created or upgraded
+   */
+  function PfModalDialog() {
+    _classCallCheck(this, PfModalDialog);
+
+    return _possibleConstructorReturn(this, (PfModalDialog.__proto__ || Object.getPrototypeOf(PfModalDialog)).call(this));
+  }
+
+  /*
+   * Called every time the element is inserted into the DOM
+   */
+
+
+  _createClass(PfModalDialog, [{
+    key: 'connectedCallback',
+    value: function connectedCallback() {
+      _pfUtils.pfUtil.addClass(this, 'modal-dialog');
+    }
+  }]);
+
+  return PfModalDialog;
+}(HTMLElement);
+
+window.customElements.define('pf-modal-dialog', PfModalDialog);
---
+++ dist/js/pf-template.js
@@ -0,0 +1,82 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "./";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	/** PfTemplateRepeaterComponent **/
+	__webpack_require__(8);
+
+/***/ },
+
+/***/ 8:
+/***/ function(module, exports) {
+
+	"use strict";
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var PFTemplate = function (_HTMLElement) {
+	  _inherits(PFTemplate, _HTMLElement);
+
+	  function PFTemplate() {
+	    _classCallCheck(this, PFTemplate);
+
+	    return _possibleConstructorReturn(this, (PFTemplate.__proto__ || Object.getPrototypeOf(PFTemplate)).apply(this, arguments));
+	  }
+
+	  return PFTemplate;
+	}(HTMLElement);
+
+	window.customElements.define("pf-template", PFTemplate);
+
+/***/ }
+
+/******/ });
